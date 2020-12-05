007358af50c5907b17271ecda0ab5e4e394abc5e
Igorplisko <igorplisko95@gmail.com>
2020-12-06 01:44:47 +0200

    Commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './styles.css'
+
+
+//!Pure javascript:________________________________________native code
+const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
+const asyncBtn = document.getElementById('async')
+const themeBtn = document.getElementById('theme')
+
+let state = 0;
+
+function render() {
+   counter.textContent = state.toString()
+}
+addBtn.addEventListener('click', () => {
+   state++
+   render()
+})
+subBtn.addEventListener('click', () => {
+   state--
+   render()
+})
+
+asyncBtn.addEventListener('click', () => {
+   setTimeout(() => {
+      state++
+      render()
+   }, 3000)
+   state--
+   render()
+})
+
+themeBtn.addEventListener('click', () => {
+   document.body.classList.toggle('dark')
+})
+
+render()
+
+//!this is the end of pure JavaScripts------------------
